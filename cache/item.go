b71da79c724d70ffdcb7a0d7708c026686ec2e71
nbxtruong <omega220@gmail.com>
2021-09-15 18:47:55 +0700

    (-) Remove old version cache services

--- cache/item.go
+++
@@ -1,7 +0,0 @@
-package cache
-
-// Item ...
-type Item struct {
-	Data    interface{} `mapstructure:"data" json:"data,omitempty" gorm:"column:data" bson:"data,omitempty" dynamodbav:"data,omitempty" firestore:"data,omitempty"`
-	Expires int64       `mapstructure:"expires" json:"expires,omitempty" gorm:"column:expires" bson:"expires,omitempty" dynamodbav:"expires,omitempty" firestore:"expires,omitempty"`
-}
